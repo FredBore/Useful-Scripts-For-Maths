7a57b9a8826281cbed0b83eab8472dd7473b6ebf
Frederick Boreham <FredBore@users.noreply.github.com>
2017-02-15 16:41:01 +0000

    Updated Adding And Subtracting Fractions.py

--- Adding And Subtracting Fractions.py
+++ Adding And Subtracting Fractions.py
@@ -9,23 +9,24 @@ numer1 = Fract1n * Fract2d
 numer2 = Fract2n * Fract1d
 
 AddorSubtract = input("Add or Subtract?")
-if AddorSubtract == "Subtract":
+ImproperorMixed = input("Improper or Mixed?")
+if AddorSubtract == "Subtract" and ImproperorMixed == "Improper":
   numer = (numer1 - numer2)
-  fraction = Fraction(numer,denom)
-  print(fraction)
-if AddorSubtract == "Add":
+  fraction = Fraction(numer, denom)
+  print("Fraction is:", fraction)
+if AddorSubtract == "Add" and ImproperorMixed == "Improper":
   numer = (numer1 + numer2)
-  fraction = Fraction(numer,denom)
-  print(fraction)
-if AddorSubtract == "Add" and denom % numer:
+  fraction = Fraction(numer, denom)
+  print("Fraction is:", fraction)
+if AddorSubtract == "Add" and ImproperorMixed == "Mixed":
   numer = (numer1 + numer2)
   numerr = (numer % denom)
   mixed = int(numer / denom)
   fraction = Fraction(numerr, denom)
-  print(mixed, fraction)
-if AddorSubtract == "Subtract" and denom % numer:
+  print("Mixed fraction is:", mixed, fraction)
+if AddorSubtract == "Subtract" and ImproperorMixed == "Mixed":
   numer = (numer1 - numer2)
   numerr = (numer % denom)
   mixed = int(numer / denom)
   fraction = Fraction(numerr, denom)
-  print(mixed, fraction)
+  print("Mixed fraction is:", mixed, fraction)
